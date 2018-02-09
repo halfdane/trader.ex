4ee845d6057e6c1875815f05152ca6c6ac9a45f2
halfdane <halfdane@gmx.net>
2018-02-09 13:45:46 +0100

    preparing to actually send an order

--- lib/trader_web/controllers/orders_controller.ex
+++ lib/trader_web/controllers/orders_controller.ex
@@ -6,12 +6,14 @@ defmodule TraderWeb.OrdersController do
                                     "buy_price" => buy_price,
                                     "lower_limit" => lower_limit,
                                     "upper_limit" => upper_limit}}) do
-    Logger.info "new order: #{symbol} min #{lower_limit} max #{upper_limit}"
+    user = conn.assigns.current_user
 
-    Trader.Order.Binance.order_limit_buy(symbol, "100%", buy_price)
-    Trader.Order.Binance.order_limit_sell(symbol, "100%", lower_limit)
-    Trader.Order.Binance.order_limit_sell(symbol, "100%", upper_limit)
+    Logger.info "new order: #{symbol} #{user.username} #{user.binance_api_key} #{conn.request_path}"
 
-    redirect(conn, to: page_path(conn, :index))
+    #Trader.Order.Binance.order_limit_buy(symbol, "100%", buy_price)
+    #Trader.Order.Binance.order_limit_sell(symbol, "100%", lower_limit)
+    #Trader.Order.Binance.order_limit_sell(symbol, "100%", upper_limit)
+
+    redirect(conn, to: coin_path(conn, :index, symbol))
   end
 end
